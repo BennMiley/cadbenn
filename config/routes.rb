16a08932b2e136b883ad2aa731b5cd594fb5541d
BennMiley <bennmiley@gmail.com>
2023-07-21 18:01:56 +0100

    ruby deleted some progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  #Rooting home page away from ruby default startup, to home
-  root 'pages#home'
-  get 'about' => 'pages#about'
-  get 'contact' => 'pages#contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
