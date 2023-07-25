725822803a04203276fc3e2edf8d0845577de711
BennMiley <bennmiley@gmail.com>
2023-07-25 18:10:54 +0100

    account creation, sign up and sign in all finished, testing working

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,19 +1,30 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  def setup
-    @user = User.new(name: "John Doe", email: "john@example.com", password: "password")
+  test "should be valid" do
+    user = User.new(
+      email: "test@example.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    assert user.valid?
   end
 
-  # ... your existing tests ...
-
-  test "password should be at least 6 characters long" do
-    @user.password = "short"
-    assert_not @user.valid?
-    assert_includes @user.errors.full_messages, "Password is too short (minimum is 6 characters)"
+  test "should require an email" do
+    user = User.new(password: "password", password_confirmation: "password")
+    assert_not user.valid?
+    assert_includes user.errors[:email], "can't be blank"
   end
 
-  test "should encrypt the password" do
-    assert_not_nil @user.encrypted_password
+  test "should require a unique email" do
+    existing_user = users(:one) # Assuming you have fixtures set up
+    user = User.new(
+      email: existing_user.email,
+      password: "password",
+      password_confirmation: "password"
+    )
+    assert_not user.valid?
+    assert_includes user.errors[:email], "has already been taken"
   end
+
 end
