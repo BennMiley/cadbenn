b6e9e8d9940a4a0fb1676204d81a7f0e173f0acc
BennMiley <bennmiley@gmail.com>
2023-07-26 21:20:29 +0100

    tweets page working, automatically assigning users to tweet

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,57 @@
+class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  # The tweet dashboard
+  def index
+    @tweets = Tweet.all
+  end
+
+  # The tweets belomnging to a given person
+  def show
+    @tweet = Tweet.find(params[:id])
+  end
+
+  # new tweet dashboard
+  def new
+    @tweet = current_user.tweets.new
+  end
+
+  # edit dasboard on existing tweet
+  def edit
+    @tweet = current_user.tweets.find(params[:id])
+  end
+
+  # post tweet
+  def create
+    @tweet = current_user.tweets.new(tweet_params)
+    if @tweet.save
+      redirect_to @tweet, notice: 'Tweet was successfully created.'
+    else
+      render :new 
+    end
+  end
+
+  # update tweet
+  def update
+    @tweet = current_user.tweets.find(params[:id])
+    if @tweet.update(tweet_params)
+      redirect_to @tweet, notice: 'Tweet was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # delete tweet
+  def destroy
+    @tweet = current_user.tweets.find(params[:id])
+    @tweet.destroy
+    redirect_to tweets_url, notice: 'Tweet was successfully destroyed.'
+  end
+
+  private
+
+    # specifies what can be changed re tweet
+    def tweet_params
+      params.require(:tweet).permit(:content, :image)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :rememberable, :validatable
 
+        has_many :tweets
+
  def self.ransackable_attributes(auth_object = nil)
   ["admin", "created_at", "email", "id", "remember_created_at", "reset_password_sent_at", "reset_password_token", "updated_at"]
 end
