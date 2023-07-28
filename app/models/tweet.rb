fcde95cf5128fa7f811c7af9bba00afefcf8ccae
BennMiley <bennmiley@gmail.com>
2023-07-28 10:51:41 +0100

    fixed admin dashboard for users, logger up and running

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,8 +2,11 @@ class Tweet < ApplicationRecord
   belongs_to :user
   acts_as_votable
 
+def self.ransackable_attributes(auth_object = nil)
+    super + %w[content created_at]
+  end
 
-    def self.ransackable_associations(auth_object = nil)
-    []
+  def self.ransackable_associations(auth_object = nil)
+    ['user']
 end
 end
